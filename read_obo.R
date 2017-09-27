0d73ae0f82502655ce0a6d414f9dba3a7669401a
ihelbig <helbigi@email.chop.edu>
2017-09-27 09:17:16 -0400

    Create read_obo.R

---
+++ read_obo.R
@@ -0,0 +1,43 @@
+
+#read HPO OBO file and tranform to table with 
+# HPO_term, descriptions, "is_a"
+
+setwd("~/Dropbox/HPO_match/HPO")
+#or wherever the hp.obo.txt is located
+
+obo <- read_lines("hp.obo.txt")
+
+num = which(obo == '[Term]')
+
+obox <- substr(obo[num+1],5,length(obo[num+1]))
+
+#start here
+obo_tab <- as.data.frame(matrix(nrow=length(obox),ncol=3))
+names(obo_tab) <- c("HPO","is_a","term")
+obo_tab[,1] <- obox
+
+obo_counter = 1
+for(i in 1:length(obo))
+{
+  if (obo[i] == "[Term]")
+  {
+    counter = i+1
+    id <- obo[counter]
+    name <- obo[counter+1]
+    obo1 <- substr(id,5,999)
+    obo2 <- substr(name,7,999)
+    while(substr(id,1,6) != 'is_a: ')
+    {
+      counter = counter+1
+      id <- obo[counter]
+      obo3 <- substr(obo[counter],7,16)
+    }
+    obo_tab[obo_counter,1] <- obo1
+    obo_tab[obo_counter,2] <- obo2
+    obo_tab[obo_counter,3] <- obo3
+    obo_counter = obo_counter+1
+    print(paste(obo1,obo2,obo3))
+  }
+  
+}
+obo_tab[1:100,]
